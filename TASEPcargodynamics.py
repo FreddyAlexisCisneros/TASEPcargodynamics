128f1f21c8e09d1fa50188c376a660a88dd45439
FreddyAlexisCisneros <58750139+FreddyAlexisCisneros@users.noreply.github.com>
2024-04-04 09:39:30 -0400

    Update TASEPcargodynamics.py

--- TASEPcargodynamics.py
+++ TASEPcargodynamics.py
@@ -1,111 +1,95 @@
+#************************************************************************
+# Written by Freddy Alexis Cisneros.                                    *
+# The following code is working properly and has been updated 4/4/2024. *
+#************************************************************************
 import numpy as np
 import random as random
 import sys
 
-def Ncomputer():
+def compute_N():
  Sum = 0
  for k in S:
-  if ms[k] != 0 and cs[k] < Nb and cs[k] > 0:
-   Sum += ms[k]*(Nb - cs[k])
+  Sum += ms[k]*(Nb - cs[k])
  return Sum
+
 def initialize_motor_distributions(M,L,name_of_ms_file):
  global u  
  m = np.loadtxt(name_of_ms_file,dtype = int)
- b = [i for i,j in enumerate(m) if j != 0 and i > 0 and i < L-1]
+ b = [ind for ind,val in enumerate(m) if val != 0 and ind > 0 and ind < L-1]
  u = sum(m[1:L-1])
- if m[0] > 0:
-  global m0
-  m0 = 1
+ global m0
+ m0 = 1 if m[0] > 0 else 0
  return m,b
+
 def initialize_cargo_distributions(L,name_of_file):
  global l,b
  c = np.loadtxt(name_of_file,dtype = int)
- C = [i for i,j in enumerate(c) if j > 0]
- A = []
- for index in C:
-  if (index + 1 < L) and (index + 1 not in C):
-   A.append(index)
+ C = [ind for ind,val in enumerate(c) if val > 0]
+ A = [ind for ind in C if ind+1 < L and ind + 1 not in C]
  b = len(C)
  l = len(A)
- c0 = 0
- if c[0] > 0:
-  c0 = 1
+ c0 = 1 if c[0] > 0 else 0
  return c,C,A,c0
+
 def initialize_S_distributions():
- global N
- S = []
  if len(ms) >= len(C):
-  for ind in C:
-   if ms[ind] > 0 and cs[ind] < Nb:
-    S.append(ind)
-    N += ms[ind]*(Nb - cs[ind])
+  S = [ind for ind in C if ms[ind] > 0 and cs[ind] < Nb]
   return S
- for ind,val in enumerate(ms):
-  if val > 0 and ind in C and cs[ind] < Nb:
-   S.append(ind)
-   N += val*(Nb - cs[ind])
+ S = [ind ind,val in enumerate(ms) if val > 0 and ind in C and cs[ind] < Nb]
  return S
+
 def add_cargo():
- global b,m0,l,c0
- motors_to_bind = random.choice(range(np.minimum(ms[0],Nb))) + 1
+ motors_to_bind = random.randint(1,np.minimum(ms[0],Nb))
  ms[0] -= motors_to_bind
  cs[0] = motors_to_bind
  C.append(0)
+ global b,m0,l,c0
  c0 = 1
  b += motors_to_bind
  if cs[1] == 0:
   A.append(0)
-  l += 1
- if ms[0] == 0:
-  m0 = 0
-  return
+  l += 1 
  if ms[0] > 0 and motors_to_bind < Nb:
   S.append(0)
+  return
+ elif ms[0] == 0: 
+  m0 = 0
+
 def move_bulk_motor_left(Site):  
  Left_site = Site - 1
  ms[Site] -= 1
  ms[Left_site] += 1
- if Site == 1:
-  global u,m0
-  u -= 1
-  m0 = 1 
-  if ms[Site] == 0:
-   B.remove(Site)
-   if Site in S:
-    S.remove(Site) 
-  if cs[Left_site] > 0 and cs[Left_site] < Nb and Left_site not in S:
-    S.append(Left_site)  
-  return
- # If the left site is still in the bulk.
  if ms[Site] == 0:
   B.remove(Site)
   if Site in S:
    S.remove(Site)
- if Left_site not in B:
-  B.append(Left_site)
  if cs[Left_site] > 0 and cs[Left_site] < Nb and Left_site not in S:
-   S.append(Left_site)
+   S.append(Left_site)  
+ if Site > 1
+  if Left_site not in B:
+   B.append(Left_site)
+  return 
+ global u,m0
+ u -= 1
+ m0 = 1 
+  
 def move_bulk_motor_right(Site): 
  Right_site = Site + 1
  ms[Site] -= 1
  ms[Right_site] += 1
- if Site == L-2:
-  global u
-  u -= 1 
-  if ms[Site] == 0:
-   B.remove(Site)
-   if Site in S:  
-    S.remove(Site)
-  if cs[Right_site] > 0 and cs[Right_site] < Nb and Right_site not in S: 
-   S.append(Right_site)
-  return 
- # If the right site is still in the bulk.
  if ms[Site] == 0:
   B.remove(Site)
   if Site in S:    
    S.remove(Site)
  if cs[Right_site] > 0 and cs[Right_site] < Nb and Right_site not in S:
    S.append(Right_site)
+ if Right_site < L-2:
+  if Right_site not in B:
+   B.append(Left_site)
+  return
+ global u
+ u -= 1 
+
 def move_left_boundary_motor(): 
  ms[0] -= 1
  ms[1] += 1
@@ -116,10 +100,12 @@ def move_left_boundary_motor():
   m0 = 0 
   if 0 in S:
    S.remove(0)
- if 1 not in B:
-  B.append(1)
- if cs[1] > 0 and cs[1] < Nb and 1 not in S:
+ if 1 in B:
+  if cs[1] > 0 and cs[1] < Nb and 1 not in S:
    S.append(1)
+  return
+ B.append(1)
+
 def move_right_boundary_motor():  
  Site = L-1
  Left_site = L-2 
@@ -127,68 +113,10 @@ def move_right_boundary_motor():
  ms[Left_site] += 1
  global u
  u += 1
- if Left_site not in B:
-  B.append(Left_site)
- if ms[Site] == 0:
-  if Site in S:  
-   S.remove(Site)
- if cs[Left_site] > 0 and cs[Left_site] < Nb and Left_site not in S:
+ if ms[Site] == 0 and Site in S:  
+  S.remove(Site) 
+ if Left_site in B:
+  if cs[Left_site] > 0 and cs[Left_site] < Nb and Left_site not in S:
    S.append(Left_site)
-
-# Finish cleaning this function up.
-def unbind_motor(x):
- global b
- ms[x] += 1
- cs[x] -= 1
- b -= 1
- 
- if x == 0:
-  global m0
-  m0 = 1
-  if cs[0] == 0:
-   global c0
-   c0 = 0
-   C.remove(0)
-   if 0 in S:
-    S.remove(0)
-   return 0
-  if 0 not in S:
-   S.append(0)
-  return -1
-  
- if x == L-1:
-  if cs[x] == 0:
-   global c0
-   c0 = 0
-   return 0
-  if 0 not in S:
-   S.append(0)
-  return -1
-  
-  
-  
-   
- if x > 0 and x < L-1:
-  global u
-  u += 1
-  if x not in B:
-   B.append(x)
- if cs[x] == 0:
-  C.remove(x)
-  if x in A:
-   global l
-   l -= 1
-   A.remove(x) 
-  if x > 0 and cs[x-1] > 0 and (x-1 not in A):
-   A.append(x-1)
-   l += 1   
- if (cs[x] == 0 or ms[x] == 0) and x in S:
-  S.remove(x)
- if cs[x] == 0:
-  global relax_time,step
-  if step >= relax_time:
-   return x   
- if cs[x] < Nb and cs[x] > 0 and ms[x] != 0:      
-  if x not in S:
-   S.append(x)
- return -1 
+  return  
+ B.append(Left_site)
